23cb5fa16fc276359a74cefc139fdd4dd0f7d31b
pilip-d <philipdell@gmx.de>
2023-05-08 23:02:30 +0200

    added stair movement logic

--- src/Player.tsx
+++ src/Player.tsx
@@ -15,6 +15,27 @@ interface PlayerArgs {
 	startPosition: Vector3;
 }
 
+function getInterpolatedStairHeight(stairLength: number, stairHeight: number, currentProgression: number) {
+	return currentProgression / (stairLength / stairHeight);
+}
+
+function getHeight(
+	lower_limit: number,
+	upper_limit: number,
+	lowerHeight: number,
+	upperHeight: number,
+	currentPos: number
+) {
+	const progression_until_3nd_platform = upper_limit - currentPos;
+	if (progression_until_3nd_platform > 0) {
+		return (
+			lowerHeight +
+			getInterpolatedStairHeight(upper_limit - lower_limit, upperHeight - lowerHeight, currentPos - lower_limit)
+		);
+	}
+	return upperHeight;
+}
+
 function Player({ startPosition }: PlayerArgs) {
 	const ref = useRef<Mesh>(null);
 
@@ -27,6 +48,19 @@ function Player({ startPosition }: PlayerArgs) {
 		if (keys.right) ref.current.position.x += speed;
 		if (keys.up) ref.current.position.z -= speed;
 		if (keys.down) ref.current.position.z += speed;
+
+		const stair_one_start = 10;
+		const stair_two_start = 6.5;
+		if (ref.current.position.x > stair_two_start) {
+			// second to third plattform
+			ref.current.position.y = getHeight(stair_two_start, 9, 1.3, 2.1, ref.current.position.x);
+		} else if (ref.current.position.z > stair_one_start) {
+			// first to second platform
+			ref.current.position.y = getHeight(stair_one_start, 16, 0.5, 1.3, ref.current.position.z);
+		} else {
+			// first plattform and default
+			ref.current.position.y = startPosition.y;
+		}
 	});
 
 	return (
