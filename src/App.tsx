6c165aa3bad83be08edf821c7576b7ba9f71bf3d
pilip-d <philipdell@gmx.de>
2023-05-08 23:25:35 +0200

    refactored and documented Stairs

--- src/App.tsx
+++ src/App.tsx
@@ -12,31 +12,44 @@ function FixedCamera() {
 	const { scene, camera } = useThree();
 	const cameraRef = useRef<PerspectiveCamera>(null);
 	useFrame(() => {
+		const distance_from_player_to_camera = 5;
 		if (!scene || !cameraRef.current) return;
 		const player = scene.getObjectByName('player');
 		if (!player) return;
 		const playerPosition = player.position;
 		if (playerPosition) {
-			camera.position.set(playerPosition.x - 5, playerPosition.y + 5, playerPosition.z - 5);
+			camera.position.set(
+				playerPosition.x - distance_from_player_to_camera,
+				playerPosition.y + distance_from_player_to_camera,
+				playerPosition.z - distance_from_player_to_camera
+			);
 			camera.lookAt(playerPosition);
 		}
 	});
 	return <perspectiveCamera ref={cameraRef} />;
 }
 
+function Stairs() {
+	// staircase orientations and positions were found by trying different values
+	return (
+		<>
+			<Stair startPosition={new Vector3(2, 0.5, 12)} orientation={[-Math.PI / 23, 0, 0]} length={7.5} />
+			<Stair
+				startPosition={new Vector3(7.5, 1.3, 23)}
+				orientation={[-Math.PI / 2, Math.PI / 2.5, Math.PI / 2]}
+				length={3}
+			/>
+		</>
+	);
+}
+
 export default function App() {
 	return (
 		<div style={{ width: '100vw', height: '100vh' }} onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} tabIndex={0}>
 			<Canvas>
-				<Stair startPosition={new Vector3(2, 0.5, 12)} orientation={[-Math.PI / 23, 0, 0]} length={7.5} />
-				<Stair
-					startPosition={new Vector3(7.5, 1.3, 23)}
-					orientation={[-Math.PI / 2, Math.PI / 2.5, Math.PI / 2]}
-					length={3}
-				/>
+				<Stairs />
 				<FixedCamera />
 				<Platform />
-				<CoordOrigin />
 				<ambientLight intensity={0.5} />
 				<Player startPosition={new Vector3(1, 0.5, 1)} />
 			</Canvas>
