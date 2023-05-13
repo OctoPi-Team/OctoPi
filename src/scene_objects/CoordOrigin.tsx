7b26dd0700016a07f491d7685c30de4bf18fa2dc
pilip-d <philipdell@gmx.de>
2023-05-13 04:25:26 +0200

    added manual position to coordorigin

--- src/scene_objects/CoordOrigin.tsx
+++ src/scene_objects/CoordOrigin.tsx
@@ -1,11 +1,15 @@
 import '../styles.css';
 import { useRef } from 'react';
-import { Mesh, BufferGeometry, Material } from 'three';
+import { Mesh, BufferGeometry, Material, Vector3 } from 'three';
 
-function CoordOrigin() {
+type CoordProps = {
+	position?: Vector3;
+}
+
+function CoordOrigin({ position = new Vector3(0, 0, 0) }: CoordProps) {
 	const originHelper = useRef<Mesh<BufferGeometry, Material | Material[]>>(null);
 	return (
-		<mesh ref={originHelper}>
+		<mesh ref={originHelper} position={position}>
 			<sphereGeometry args={[0.1, 8, 8]} />
 			<meshBasicMaterial color="red" />
 		</mesh>
