994d36d529f9008cf41d679f8608e6a5c1d236d9
pilip-d <philipdell@gmx.de>
2023-05-17 15:55:15 +0200

    updated config files for testing

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	testMatch: ['**/__test__/**/*.(ts|tsx|js)'],
+	moduleNameMapper: {
+		'\\.(css|less)$': 'identity-obj-proxy',
+	},
+	reporters: [
+		'default',
+		[
+			'./node_modules/jest-html-reporter',
+			{
+				pageTitle: 'Test Report',
+			},
+		],
+	],
+};
