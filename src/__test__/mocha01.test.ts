64ea3b08f98610b18980262b6de72637e5d4157e
pilip-d <philipdell@gmx.de>
2023-05-17 15:54:25 +0200

    added simpe tests

---
+++ src/__test__/mocha01.test.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Video from '../Video';
+
+describe('VideoTest', function () {
+	it('VideoContainer', async function () {
+		render(React.createElement(Video));
+
+		const videoElement = screen.getByTestId('start-animation-video');
+		expect(videoElement.childNodes).toHaveLength(1);
+	});
+
+	it('VideoSource', async function () {
+		render(React.createElement(Video));
+
+		const sourceElement = screen.getByTestId('start-animation-source');
+		expect(sourceElement.getAttribute('src')).toBe('start_animation.mp4');
+	});
+});
