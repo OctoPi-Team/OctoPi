d75b7223ae4c1dcab37e5a667d5edab5c9f53479
Julian Wernz <julian24.12@gmx.de>
2023-05-17 16:37:36 +0200

    tube

--- src/scene_objects/Tube.tsx
+++ src/scene_objects/Tube.tsx
@@ -1,5 +1,18 @@
 import { useRef, useEffect } from 'react';
-import { Color, Euler, Mesh, Vector3 , Vector2} from 'three';
+import THREE, {
+	Color,
+	Euler,
+	Mesh,
+	Vector3,
+	Vector2,
+	MathUtils,
+	Material,
+	BufferGeometry,
+	MeshStandardMaterial,
+	SphereGeometry,
+	TubeGeometry,
+	CatmullRomCurve3,
+} from 'three';
 import { Text } from '@react-three/drei';
 import { useFrame, useThree } from '@react-three/fiber';
 
@@ -8,16 +21,33 @@ type TubeProps = {
 	position: [number, number, number];
 	size?: [number, number, number];
 	color?: number | string | THREE.Color;
-	rotation: number;
+	rotation: [number, number, number];
 	reference?: (meshRef: THREE.Mesh<THREE.BufferGeometry, THREE.Material | THREE.Material[]>) => void;
 };
+function Tube({
+	name = 'Tube',
+	position = [0, 0, 0],
+	size = [1, 1, 1],
+	color = 'white',
+	rotation = [0, 0, 0],
+	reference,
+}: TubeProps): JSX.Element {
+	const curve = new CatmullRomCurve3([
+		new Vector3(0, 0, 0),
+		new Vector3(0, 7, 0),
+		new Vector3(-10.5, 7, 5),
+		new Vector3(-10.6, 5.5, 5),
+	]);
 
-function Tube({name, position = [0, 0, 0], size = [1, 0.1, 1], color = "white", rotation, reference }: TubeProps) {
-	return (	
-		<mesh position={position}>
-			<cylinderBufferGeometry attach="geometry" args={[size[0], size[0], size[1], 32]} />
-			<meshStandardMaterial color={color} transparent opacity={0.5}/>
-		</mesh>
+	const tubeGeometry = new TubeGeometry(curve, 100, 0.4, 100, false);
+
+	return (
+		<>
+			<mesh name={name} position={position}>
+				<primitive object={tubeGeometry} />
+				<meshStandardMaterial color={color} transparent opacity={0.5} />
+			</mesh>
+		</>
 	);
 }
 
