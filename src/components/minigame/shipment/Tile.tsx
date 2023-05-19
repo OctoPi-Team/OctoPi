b9b161d0de510d78d556c403f893d329f89683ee
Julian Wernz <julian24.12@gmx.de>
2023-05-19 17:16:33 +0200

    tubes added * with right angle

--- src/components/minigame/shipment/Tile.tsx
+++ src/components/minigame/shipment/Tile.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { Mesh, Vector3 } from 'three';
+import Tube from '../../overworld/objects/Tube';
 
 export type TileProps = {
 	gridPosition: [number, number];
@@ -8,7 +9,12 @@ export type TileProps = {
 
 const GRID_SPACING = 0.5;
 const TILE_SIZE = 3;
+const randomVectorX = new Vector3(0, 0, -TILE_SIZE / 2);
+const randomVectorZ = new Vector3(0, 0, TILE_SIZE / 2 - 0.4); // if right angle the z axis - 0.4
+const rightAngleVector = new Vector3(TILE_SIZE / 2, 0, Math.PI / 2 - 0.4);
 
+// Rotate the vector by 90 degrees around the y-axis to make it a right angle vector
+//rightAngleVector.applyAxisAngle(new Vector3(0, 1, 0), Math.PI / 2);
 function getRealPositionFromGridPosition(gridPosition: [number, number]): Vector3 {
 	return new Vector3(
 		gridPosition[0] * GRID_SPACING + gridPosition[0] * TILE_SIZE,
@@ -33,6 +39,7 @@ export default function Tile({ gridPosition, tileClickHandler }: TileProps) {
 				onClick={() => {
 					if (tileClickHandler) tileClickHandler({ gridPosition: gridPosition });
 				}}>
+				<Tube position={[0, 0, 0]} color={'black'} vectors={[randomVectorX, randomVectorZ, rightAngleVector]} />
 				<boxGeometry args={[TILE_SIZE, 0.5, TILE_SIZE]} />
 				<meshStandardMaterial color={'yellow'} />
 			</mesh>
