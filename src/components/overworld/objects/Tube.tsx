c120d729e4c785aa63a85bf39302d569f0888b83
Julian Wernz <julian24.12@gmx.de>
2023-05-19 16:27:54 +0200

    updated tube

--- src/components/overworld/objects/Tube.tsx
+++ src/components/overworld/objects/Tube.tsx
@@ -5,16 +5,16 @@ type TubeProps = {
 	position: [number, number, number];
 	size?: [number, number, number];
 	color?: number | string | THREE.Color;
-	rotation: [number, number, number];
 	reference?: (meshRef: THREE.Mesh<THREE.BufferGeometry, THREE.Material | THREE.Material[]>) => void;
+	vectors?: [...Vector3[]];
 };
-function Tube({ name = 'Tube', position = [0, 0, 0], color = 'white' }: TubeProps): JSX.Element {
-	const curve = new CatmullRomCurve3([
-		new Vector3(0, 0, 0),
-		new Vector3(0, 7, 0),
-		new Vector3(-10.5, 7, 5),
-		new Vector3(-10.6, 5.5, 5),
-	]);
+function Tube({
+	name = 'Tube',
+	position = [0, 0, 0],
+	color = 'white',
+	vectors = [new Vector3(0, 0, 0), new Vector3(0, 7, 0), new Vector3(-10.5, 7, 5), new Vector3(-10.6, 5.5, 5)],
+}: TubeProps): JSX.Element {
+	const curve = new CatmullRomCurve3(vectors);
 
 	const tubeGeometry = new TubeGeometry(curve, 100, 0.4, 100, false);
 
