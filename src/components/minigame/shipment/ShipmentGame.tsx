8d54e55b897dafb5d542910277e5ba1214b7c8ed
Julian Wernz <julian24.12@gmx.de>
2023-05-23 09:15:08 +0200

    fixed position of input tube

--- src/components/minigame/shipment/ShipmentGame.tsx
+++ src/components/minigame/shipment/ShipmentGame.tsx
@@ -41,7 +41,7 @@ export default function ShipMentMinigame({ setSceneHook }: SceneProps) {
 							position={[0, 2, 0]}
 							color="#3aaa35"
 							vectors={[
-								new Vector3(-1.8, -1.5, 6.4),
+								new Vector3(-1.8, -1.3, 6.4),
 								new Vector3(-3, -1.2, 6.4),
 								new Vector3(-3.5, 2, 6.4),
 								new Vector3(-8, 2, 6.4),
