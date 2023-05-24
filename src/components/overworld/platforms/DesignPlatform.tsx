eb4949edf260adeb02c769ac785b14fd27e4b69b
2120914 <2120914@stud.hs-mannheim.de>
2023-05-24 21:23:22 +0200

    add file DesignPlatform

---
+++ src/components/overworld/platforms/DesignPlatform.tsx
@@ -0,0 +1,29 @@
+import { Color } from 'three';
+import ObjectLoad from '../../ObjectLoad';
+import SimplePlatform from './SimplePlatform';
+
+type DesignPlatformOptions = {
+	position?: [number, number, number];
+	reference?: (meshRef: THREE.Mesh<THREE.BufferGeometry, THREE.Material | THREE.Material[]>) => void;
+};
+
+export default function DesignPlatform({ position = [0, 0, 0], reference }: DesignPlatformOptions): JSX.Element {
+	return (
+		<>
+			<SimplePlatform
+				name={'Design'}
+				position={position}
+				size={[14, 0.5, 17]}
+				reference={reference}
+				color={new Color(0xdaf0ee)}
+			/>
+			<ObjectLoad
+				pathObj="/Whiteboard/whiteboard.obj"
+				pathMtl="/Whiteboard/whiteboard.mtl"
+				position={[5.6, position[1], position[2] - 1]}
+				scale={[0.1, 0.1, 0.1]}
+				rotation={[0, 0, 0]}
+			/>
+		</>
+	);
+}
