09c212293664cc543ae1e8d55f439293368df754
Julian Wernz <julian24.12@gmx.de>
2023-06-09 15:22:28 +0200

    production platform object size fixed

--- src/components/overworld/platforms/ProductionPlatform.tsx
+++ src/components/overworld/platforms/ProductionPlatform.tsx
@@ -21,59 +21,28 @@ export default function ProductionPlatform({
 			<ObjectLoad
 				path="/Roboterarm_kaputt/roboterarm_kaputt.glb"
 				position={[position[0] - 2, position[1], position[2] + 2]}
-				scale={[0.1, 0.1, 0.1]}
+				scale={[0.09, 0.09, 0.09]}
 				rotation={[0, 0, 0]}
 				collisionRefSetter={addCollisionBox}
 			/>
 			<Text text={'Production'} position={[position[0] + 2, position[1], position[2] + 7]} rotation={[0, 180, 0]} />
 			<gridHelper position={[position[0] - 3, position[1], position[2]]} args={[10, 10, 'black', 'white']} />
 
-			<group>
-				<ObjectLoad
-					path="/Notebook/notebook.glb"
-					position={[position[0] + 4, position[1] + 6, position[2] - 5]}
-					scale={[0.28, 0.28, 0.28]}
-					rotation={[0, 150, 0]}
-					collisionRefSetter={addCollisionBox}
-				/>
-				<ObjectLoad
-					path="/Schreibtisch/schreibtisch.glb"
-					position={[position[0] + 8, position[1], position[2] - 2]}
-					scale={[0.5, 0.5, 0.25]}
-					rotation={[0, 90, 0]}
-					collisionRefSetter={addCollisionBox}
-				/>
-				<ObjectLoad
-					path="/Drehstuhl/drehstuhl.glb"
-					position={[position[0] + 7, position[1], position[2] - 1.5]}
-					scale={[0.25, 0.25, 0.25]}
-					rotation={[0, 160, 0]}
-					collisionRefSetter={addCollisionBox}
-				/>
-			</group>
-			<group name="corner-desk">
-				<ObjectLoad
-					path="/Schreibtisch/schreibtisch.glb"
-					position={[position[0] + 8, position[1], position[2] + 3]}
-					scale={[0.4, 0.4, 0.4]}
-					rotation={[0, 90, 0]}
-					collisionRefSetter={addCollisionBox}
-				/>
-				<ObjectLoad
-					path="/Drehstuhl/drehstuhl.glb"
-					position={[position[0] + 7, position[1], position[2] + 3]}
-					scale={[0.22, 0.22, 0.22]}
-					rotation={[0, 170, 0]}
-					collisionRefSetter={addCollisionBox}
-				/>
-				<ObjectLoad
-					path="/Notebook/notebook.glb"
-					position={[position[0] + 7, position[1] + 2.5, position[2] + 3]}
-					scale={[0.25, 0.25, 0.25]}
-					rotation={[0, 140, 0]}
-					collisionRefSetter={addCollisionBox}
-				/>
-			</group>
+			<ObjectLoad
+				path="/SchreibtischMitStuhl/schreibtischMitStuhl.glb"
+				position={[position[0] + 8, position[1], position[2] - 2]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 90, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+
+			<ObjectLoad
+				path="/SchreibtischMitStuhl/schreibtischMitStuhl.glb"
+				position={[position[0] + 8, position[1], position[2] + 3]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 90, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
 		</>
 	);
 }
