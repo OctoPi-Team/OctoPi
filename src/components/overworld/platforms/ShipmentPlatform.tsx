c7635c19127584031f13a78985d95726207e4f2d
Julian Wernz <julian24.12@gmx.de>
2023-06-10 17:48:18 +0200

    added another tube to shipment platform

--- src/components/overworld/platforms/ShipmentPlatform.tsx
+++ src/components/overworld/platforms/ShipmentPlatform.tsx
@@ -5,7 +5,7 @@ import Button from '../objects/Button';
 import { SHIPMENT } from '../../../AllColorVariables';
 import Tube from '../objects/Tube';
 import Text from '../../Text';
-import { Box3 } from 'three';
+import { Box3, Vector3 } from 'three';
 
 type ShipmentPlatformOptions = {
 	position?: [number, number, number];
@@ -119,6 +119,12 @@ export default function ShipmentPlatform({
 				collisionRefSetter={addCollisionBox}
 			/>
 			<Tube name="Tube" position={[position[0] + 7.6, position[1] - 1.9, position[2] + 0.5]} size={[0.5, 8, 1]} />
+			<Tube
+				name="Tube2"
+				position={[position[0] - 7, position[1] - 1.9, position[2] - 7]}
+				size={[0.5, 8, 1]}
+				vectors={[new Vector3(0, 0, 0), new Vector3(0, 3, 0), new Vector3(0, 3, 7), new Vector3(0, 0, 7)]}
+			/>
 			<Button position={[position[0] - 6, position[1] + 6, position[2] - 9]} reference={buttonreference} />
 		</>
 	);
