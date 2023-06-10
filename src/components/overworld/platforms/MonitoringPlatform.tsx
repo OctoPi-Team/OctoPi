b55d6f1dda8b45fb3d43210d016ffce0a4ca6338
Julian Wernz <julian24.12@gmx.de>
2023-06-10 18:10:26 +0200

    tubes added to  monitoring

--- src/components/overworld/platforms/MonitoringPlatform.tsx
+++ src/components/overworld/platforms/MonitoringPlatform.tsx
@@ -2,7 +2,8 @@ import ObjectLoad from '../../ObjectLoad';
 import SimplePlatform from './SimplePlatform';
 import { MONITORING } from '../../../AllColorVariables';
 import Text from '../../Text';
-import { Box3 } from 'three';
+import { Box3, Vector3 } from 'three';
+import Tube from '../objects/Tube';
 
 type MonitoringPlatformOptions = {
 	position?: [number, number, number];
@@ -32,6 +33,23 @@ export default function MonitoringPlatform({
 				scale={[0.45, 0.45, 0.45]}
 				rotation={[0, 7, 0]}
 				collisionRefSetter={addCollisionBox}
+			/>
+			<ObjectLoad
+				path="/TischMitTV/tischMitTV.glb"
+				position={[position[0] + 2.5, position[1], position[2] - 4.5]}
+				scale={[0.45, 0.45, 0.45]}
+				rotation={[0, 30, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<Tube
+				position={[position[0] - 5, position[1] - 1.9, position[2] + 9]}
+				size={[0.5, 8, 1]}
+				vectors={[new Vector3(0, 0, 0), new Vector3(0, 3, 0), new Vector3(6, 3, 0), new Vector3(6, 0, 0)]}
+			/>
+			<Tube
+				position={[position[0] - 5, position[1] - 1.9, position[2] - 8]}
+				size={[0.5, 8, 1]}
+				vectors={[new Vector3(0, 0, 0), new Vector3(0, 4, 0), new Vector3(0, 4, 3), new Vector3(0, 0, 3)]}
 			/>
 		</>
 	);
