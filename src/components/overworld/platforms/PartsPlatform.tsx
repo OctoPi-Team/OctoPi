a6bc4160bdf8c3c43faead862f5685304b23eabc
Julian Wernz <julian24.12@gmx.de>
2023-06-10 17:44:27 +0200

    shelf and tube added to parts

--- src/components/overworld/platforms/PartsPlatform.tsx
+++ src/components/overworld/platforms/PartsPlatform.tsx
@@ -2,7 +2,8 @@ import { PARTS } from '../../../AllColorVariables';
 import ObjectLoad from '../../ObjectLoad';
 import SimplePlatform from './SimplePlatform';
 import Text from '../../Text';
-import { Box3 } from 'three';
+import { Box3, Vector3 } from 'three';
+import Tube from '../objects/Tube';
 
 type PartsPlatformOptions = {
 	position?: [number, number, number];
@@ -19,19 +20,64 @@ export default function PartsPlatform({
 		<>
 			<SimplePlatform position={position} size={[24, 0.5, 18]} reference={reference} color={PARTS} />
 			<Text text={'Parts'} position={[position[0] - 10, position[1] + 10, position[2]]} rotation={[0, 180, 0]} />
-			<ObjectLoad
-				path="/Metallregal/metallregal.glb"
-				position={[position[0], position[1], position[2] - 1]}
-				scale={[0.3, 0.3, 0.3]}
-				rotation={[0, 0, 0]}
-				collisionRefSetter={addCollisionBox}
-			/>
-			<ObjectLoad
-				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
-				position={[position[0] + 5, position[1], position[2] + 2]}
-				scale={[0.3, 0.3, 0.3]}
-				rotation={[0, 90, 0]}
-				collisionRefSetter={addCollisionBox}
+			<group name="metallregale">
+				<ObjectLoad
+					path="/Metallregal/metallregal.glb"
+					position={[position[0], position[1], position[2] - 5]}
+					scale={[0.3, 0.3, 0.3]}
+					rotation={[0, 0, 0]}
+					collisionRefSetter={addCollisionBox}
+				/>
+				<ObjectLoad
+					path="/Metallregal/metallregal.glb"
+					position={[position[0] + 3, position[1], position[2] - 5]}
+					scale={[0.3, 0.3, 0.3]}
+					rotation={[0, 0, 0]}
+					collisionRefSetter={addCollisionBox}
+				/>
+				<ObjectLoad
+					path="/Metallregal/metallregal.glb"
+					position={[position[0] - 3, position[1], position[2] - 5]}
+					scale={[0.3, 0.3, 0.3]}
+					rotation={[0, 0, 0]}
+					collisionRefSetter={addCollisionBox}
+				/>
+				<ObjectLoad
+					path="/Metallregal/metallregal.glb"
+					position={[position[0] - 6, position[1], position[2] - 5]}
+					scale={[0.3, 0.3, 0.3]}
+					rotation={[0, 0, 0]}
+					collisionRefSetter={addCollisionBox}
+				/>
+			</group>
+			<group name="kaputteMetallregale">
+				<ObjectLoad
+					path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+					position={[position[0] + 7, position[1], position[2] + 2]}
+					scale={[0.3, 0.3, 0.3]}
+					rotation={[0, 90, 0]}
+					collisionRefSetter={addCollisionBox}
+				/>
+				<ObjectLoad
+					path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+					position={[position[0] + 7, position[1], position[2] + 5]}
+					scale={[0.3, 0.3, 0.3]}
+					rotation={[0, 90, 0]}
+					collisionRefSetter={addCollisionBox}
+				/>
+				<ObjectLoad
+					path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+					position={[position[0] + 7, position[1], position[2] - 1]}
+					scale={[0.3, 0.3, 0.3]}
+					rotation={[0, 90, 0]}
+					collisionRefSetter={addCollisionBox}
+				/>
+			</group>
+			<Tube
+				name="Tube"
+				position={[position[0] + 11, position[1] - 1.9, position[2] - 7]}
+				size={[0.5, 8, 1]}
+				vectors={[new Vector3(0, 0, 0), new Vector3(0, 3, 0), new Vector3(-5, 3, 0), new Vector3(-5, 0, 0)]}
 			/>
 		</>
 	);
