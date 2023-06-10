572941a75ea70912445cd8595d2e5c5b7cf821bb
Jasmin <jasmin.tschernoch@stud.hs-mannheim.de>
2023-06-10 11:06:48 +0200

    platfroms with round edges

--- src/components/overworld/platforms/SimplePlatform.tsx
+++ src/components/overworld/platforms/SimplePlatform.tsx
@@ -1,14 +1,15 @@
 import { useRef, useEffect, useState } from 'react';
-import { useFrame, useThree, Color } from '@react-three/fiber';
-import { Box3, Vector3 } from 'three';
+import { useFrame, useThree} from '@react-three/fiber';
+import { Box3, Mesh, MeshStandardMaterial, Vector3, Color } from 'three';
 import { SimpleText } from './SimpleText';
+import {RoundedBoxGeometry} from 'three/examples/jsm/geometries/RoundedBoxGeometry';
 
 // This interface is used to set the options of the ObjectLoad function.
 type SimplePlatformProps = {
 	name?: string;
 	position: [number, number, number];
 	size?: [number, number, number];
-	color: Color;
+	color: string;
 	reference?: (meshRef: Box3) => void;
 };
 
@@ -16,6 +17,7 @@ type SimplePlatformProps = {
 export default function SimplePlatform({ name, position, size = [1, 0.1, 1], color, reference }: SimplePlatformProps) {
 	const ref = useRef<THREE.Mesh>(null);
 	const [collsionRefWasSet, collsionRefSet] = useState(false);
+	
 
 	if (!collsionRefWasSet && reference && ref.current) {
 		collsionRefSet(true);
@@ -26,7 +28,7 @@ export default function SimplePlatform({ name, position, size = [1, 0.1, 1], col
 			const meshPosition = new Vector3(position[0], position[1] - size[1] / 2, position[2]);
 			ref.current.position.copy(meshPosition);
 		}
-	}, position);
+	});
 
 	const { camera } = useThree();
 	const meshRef = useRef<THREE.Mesh>(null);
@@ -37,14 +39,20 @@ export default function SimplePlatform({ name, position, size = [1, 0.1, 1], col
 			textRef.current.lookAt(camera.position);
 		}
 	});
+	
+	const roundedBoxGeometry = new RoundedBoxGeometry(size[0], size[1], size[2], 4, 2);
+	const roundedBoxMaterial = new MeshStandardMaterial({color});
+	
+	const roundedBoxMesh = new Mesh(roundedBoxGeometry, roundedBoxMaterial);
+  	//roundedBoxMesh.position.set(0, 0, 0);
 
 	return (
 		<>
 			<SimpleText position={position} textValue={name} />
 			<mesh ref={ref} castShadow receiveShadow>
-				<boxGeometry args={size} />
-				<meshStandardMaterial color={color} />
+				 <primitive object={roundedBoxMesh} />
 			</mesh>
 		</>
 	);
 }
+
