62cef4be14131dfadcca0eb70b6ebec8a4ef3061
Jasmin <jasmin.tschernoch@stud.hs-mannheim.de>
2023-06-11 20:30:43 +0200

    placed objects on parts platform

--- src/components/overworld/platforms/PartsPlatform.tsx
+++ src/components/overworld/platforms/PartsPlatform.tsx
@@ -24,16 +24,116 @@ export default function PartsPlatform({
 			<Text text={'Parts'} position={[position[0] - 10, position[1] + 10, position[2]]} rotation={[0, 180, 0]} />
 			<ObjectLoad
 				path="/Metallregal/metallregal.glb"
-				position={[position[0], position[1], position[2] - 1]}
+				position={[position[0] - 1, position[1], position[2] - 2.5]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 0, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<ObjectLoad
+				path="/Metallregal/metallregal.glb"
+				position={[position[0] - 1, position[1], position[2] - 6.5]}
 				scale={[0.3, 0.3, 0.3]}
 				rotation={[0, 0, 0]}
 				collisionRefSetter={addCollisionBox}
 			/>
 			<ObjectLoad
 				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
-				position={[position[0] + 5, position[1], position[2] + 2]}
+				position={[position[0] - 6, position[1], position[2] - 2.5]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 0, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<ObjectLoad
+				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+				position={[position[0] - 6, position[1], position[2] - 6.5]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 0, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<ObjectLoad
+				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+				position={[position[0] + 10, position[1], position[2] + 4]}
 				scale={[0.3, 0.3, 0.3]}
 				rotation={[0, 90, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+				<ObjectLoad
+				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+				position={[position[0] + 10, position[1], position[2]]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 90, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+				<ObjectLoad
+				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+				position={[position[0] + 10, position[1], position[2] - 4]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 90, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<ObjectLoad
+				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+				position={[position[0] + 6, position[1], position[2] + 4]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 90, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+				<ObjectLoad
+				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+				position={[position[0] + 6, position[1], position[2]]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 90, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+				<ObjectLoad
+				path="/kaputtesMetallregal/kaputtesMetallregal.glb"
+				position={[position[0] + 4, position[1], position[2] - 2.5]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 0, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+				<ObjectLoad
+				path="/Palette/palette.glb"
+				position={[position[0] + 10, position[1], position[2] +-7]}
+				scale={[0.15, 0.15, 0.15]}
+				rotation={[0, 0, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<group>
+			<ObjectLoad
+				path="/Palette/palette.glb"
+				position={[position[0] + 10, position[1], position[2] + 7]}
+				scale={[0.15, 0.15, 0.15]}
+				rotation={[0, 90, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<ObjectLoad
+				path="/Packet/packet.glb"
+				position={[position[0] + 8, position[1] + 2, position[2] + 5]}
+				scale={[0.1, 0.1, 0.1]}
+				rotation={[0, 0, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<ObjectLoad
+				path="/Paket_2/paket_2.glb"
+				position={[position[0] + 8, position[1] + 2, position[2] + 6]}
+				scale={[0.1, 0.1, 0.1]}
+				rotation={[0, 0, 0]}
+				collisionRefSetter={addCollisionBox}
+			/>
+			<ObjectLoad
+					path="/UmgeknicktesPaket/umgeknicktesPaket.glb"
+					position={[position[0] + 9, position[1] + 2, position[2] + 5.5]}
+					scale={[0.1, 0.1, 0.1]}
+					rotation={[0, 8, 0]}
+					collisionRefSetter={addCollisionBox}
+				/>
+			</group>
+			<ObjectLoad
+				path="/SchreibtischMitStuhl/schreibtischMitStuhl.glb"
+				position={[position[0] - 10, position[1], position[2] - 8]}
+				scale={[0.3, 0.3, 0.3]}
+				rotation={[0, 180, 0]}
 				collisionRefSetter={addCollisionBox}
 			/>
 			<Button position={[position[0] - 9, position[1] + 6, position[2] - 6]} reference={buttonreference} />
