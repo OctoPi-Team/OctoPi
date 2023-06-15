746612b52353f1516cb019019223a3bbbcc39847
Jasmin <jasmin.tschernoch@stud.hs-mannheim.de>
2023-06-15 12:00:36 +0200

    ball animation in tube

--- src/components/overworld/objects/Tube.tsx
+++ src/components/overworld/objects/Tube.tsx
@@ -1,4 +1,4 @@
-import THREE, { Vector3, TubeGeometry, CatmullRomCurve3, DoubleSide } from 'three';
+import THREE, { CatmullRomCurve3, DoubleSide, Mesh, MeshBasicMaterial, SphereGeometry, TubeGeometry, Vector3 } from 'three';
 
 type TubeProps = {
 	name?: string;
@@ -8,21 +8,49 @@ type TubeProps = {
 	reference?: (meshRef: THREE.Mesh<THREE.BufferGeometry, THREE.Material | THREE.Material[]>) => void;
 	vectors?: [...Vector3[]];
 	detailed?: boolean;
+	ballAnimation?: boolean;
 };
 function Tube({
 	name = 'Tube',
 	position = [0, 0, 0],
 	vectors = [new Vector3(0, 0, 0), new Vector3(0, 6, 0), new Vector3(-10.5, 6, 5), new Vector3(-10.6, 4.5, 5)],
 	detailed = false,
+	ballAnimation = false,
 }: TubeProps): JSX.Element {
 	const curve = new CatmullRomCurve3(vectors);
 	const tubeGeometry = new TubeGeometry(curve, 100, 0.4, detailed ? 50 : 10, false);
+	//const material = new MeshPhysicalMaterial({roughness: 0.01, transmission: 1, thickness: 1, side: DoubleSide });
+
+	const ballRadius = 0.25;
+	const ballGeometry = new SphereGeometry(ballRadius, 32, 32);
+  	const ballMaterial = new MeshBasicMaterial({ color: 0xff0000 });
+  	const ballMesh = new Mesh(ballGeometry, ballMaterial);
+
+  	const ballSpeed = 1; 
+ 
+	let ballPosition = 0; 
+
+  const animateBall = () => {
+    requestAnimationFrame(animateBall);
+
+    const tubePosition = curve.getPointAt(ballPosition % 1);
+
+    ballMesh.position.copy(tubePosition);
+
+    ballPosition += ballSpeed * 0.001;
+  };
+  if (ballAnimation) {
+    animateBall();
+  }
 	return (
 		<>
 			<mesh name={name} position={position}>
 				<primitive object={tubeGeometry} />
 				<meshPhysicalMaterial roughness={0.01} transmission={1} thickness={1} side={DoubleSide} />
 			</mesh>
+			{ballAnimation &&
+			 <primitive object={ballMesh}/>
+}
 		</>
 	);
 }
