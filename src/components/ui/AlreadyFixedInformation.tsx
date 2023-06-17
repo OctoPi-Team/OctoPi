c5da4f94255f34ade9ee3d5408a4a60f2ae4ae3b
timowenz <2025014@stud.hs-mannheim.de>
2023-06-17 16:46:47 +0200

    Changed styling

--- src/components/ui/AlreadyFixedInformation.tsx
+++ src/components/ui/AlreadyFixedInformation.tsx
@@ -7,12 +7,14 @@ type Props = {
 function AlreadyFixedInformation({ isPlatformFixed }: Props) {
 	return (
 		<div className={'alreadyfixedinformation'}>
-			<div className={'platformnames'}> Parts {isPlatformFixed?.parts ? <div>✅</div> : <div>❌</div>}</div>
+			<div className={'platformnamesalign'}>
+				<div className={'platformnames'}> Parts {isPlatformFixed?.parts ? <div>✅</div> : <div>❌</div>}</div>
+			</div>
+			<div className={'platformnames'}> Design {isPlatformFixed?.design ? <div>✅</div> : <div>❌</div>}</div>
 			<div className={'platformnames'}> Shipment {isPlatformFixed?.shipment ? <div>✅</div> : <div>❌</div>}</div>
 			<div className={'platformnames'}> Monitoring {isPlatformFixed?.monitoring ? <div>✅</div> : <div>❌</div>}</div>
-			<div className={'platformnames'}> Design {isPlatformFixed?.design ? <div>✅</div> : <div>❌</div>}</div>
-			<div className={'platformnames'}> Engineering {isPlatformFixed?.engineering ? <div>✅</div> : <div>❌</div>}</div>
 			<div className={'platformnames'}> Production {isPlatformFixed?.production ? <div>✅</div> : <div>❌</div>}</div>
+			<div className={'platformnames'}> Engineering {isPlatformFixed?.engineering ? <div>✅</div> : <div>❌</div>}</div>
 		</div>
 	);
 }
