7fabff698deec2b58244d9fb47b2e6efd23b26da
Julian Wernz <julian24.12@gmx.de>
2023-06-21 14:08:55 +0200

    random color for each platform and bigger

--- src/components/overworld/objects/Tube.tsx
+++ src/components/overworld/objects/Tube.tsx
@@ -7,8 +7,8 @@ import THREE, {
 	TubeGeometry,
 	Vector3,
 } from 'three';
-import { GREEN } from '../../../AllColorVariables';
-
+import { GREEN, RED, BLUE, PINK } from '../../../AllColorVariables';
+const COLORS = [GREEN, RED, BLUE, PINK];
 type TubeProps = {
 	name?: string;
 	position: [number, number, number];
@@ -26,13 +26,13 @@ function Tube({
 	vectors = [new Vector3(0, 0, 0), new Vector3(0, 6, 0), new Vector3(-10.5, 6, 5), new Vector3(-10.6, 4.5, 5)],
 	detailed = false,
 	ballAnimation = false,
-	ballColor = GREEN,
+	ballColor = COLORS[Math.floor(Math.random() * COLORS.length)],
 }: TubeProps): JSX.Element {
 	const curve = new CatmullRomCurve3(vectors);
 	const tubeGeometry = new TubeGeometry(curve, 1000, 0.4, detailed ? 50 : 15, false);
 	//const material = new MeshPhysicalMaterial({roughness: 0.01, transmission: 1, thickness: 1, side: DoubleSide });
 
-	const ballRadius = 0.25;
+	const ballRadius = 0.3;
 	const ballGeometry = new SphereGeometry(ballRadius, 32, 32);
 	const ballMaterial = new MeshBasicMaterial({ color: ballColor });
 	const ballMesh = new Mesh(ballGeometry, ballMaterial);
@@ -43,7 +43,6 @@ function Tube({
 
 	const animateBall = () => {
 		requestAnimationFrame(animateBall);
-
 		const tubePosition = curve.getPointAt(ballPosition % 1);
 		ballMesh.position.copy(tubePosition);
 		ballPosition += ballSpeed * 0.001;
