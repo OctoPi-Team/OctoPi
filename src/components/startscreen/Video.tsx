30bdc88f0c99e84982b46f611b0fd88e99f251a9
pilip-d <philipdell@gmx.de>
2023-06-21 13:04:50 +0200

    changed video to mp4

--- src/components/startscreen/Video.tsx
+++ src/components/startscreen/Video.tsx
@@ -9,16 +9,14 @@ export default function Video({ setVisible }: VideoArgs) {
 		<React.Fragment>
 			<video
 				data-testid="start-animation-video"
-				loop
 				className="video"
-
 				height={window.innerHeight}
 				width={window.innerWidth}
 				preload="auto"
 				autoPlay
 				muted
 				data-setup="{}">
-				<source data-testid="start-animation-source" src="Startanimation.mov" type="video/mp4"></source>
+				<source data-testid="start-animation-source" src="Startanimation.mp4" type="video/mp4"></source>
 			</video>
 		</React.Fragment>
 	);
