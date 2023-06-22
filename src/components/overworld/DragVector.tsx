609c5757b329bf479620d734b83bacd6a4ad2c3f
timowenz <2025014@stud.hs-mannheim.de>
2023-06-22 11:02:16 +0200

    Fixed a bug where you couldnt handle movement after you step on a button

--- src/components/overworld/DragVector.tsx
+++ src/components/overworld/DragVector.tsx
@@ -1,9 +1,10 @@
 import { Vector2 } from 'three';
 
+let movingState = false;
 const DragVector = (startPos: Vector2, change: (v: Vector2) => void) => {
-	let movingState = false;
 	const handleMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
 		if (!startPos || !movingState) return;
+		console.log('Hello');
 		const { clientX, clientY } = event;
 		const dragVector = new Vector2(clientX - startPos.x, startPos.y - clientY);
 		change(dragVector);
