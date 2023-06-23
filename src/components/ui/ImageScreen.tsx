b3dc74ec35ed5cfb1cbf903981fcd4a4d1d2b62b
Julian Wernz <julian24.12@gmx.de>
2023-06-23 10:36:33 +0200

    added second heading

--- src/components/ui/ImageScreen.tsx
+++ src/components/ui/ImageScreen.tsx
@@ -20,13 +20,14 @@ export default function ImageScreen({
 	const [text, setText] = useState('Bitte warten');
 
 	useEffect(() => {
-		if (opacity === 1) setText('Klicke um fortzufahren');
+		if (opacity === 1) setText('Klicke, um fortzufahren');
 	}, [opacity]);
 
 	return (
 		<div onClick={onclick} style={{ opacity: opacity }}>
 			<img className="imgScreen" src={imageSource} />
-			<h1 className={`imgScreenText ${opacity != 1 ? 'dot-animation' : 'pump-animation'}`}>{text}</h1>
+			<h1 className={`img-screen-text-heading`}>Operation: Innovation</h1>
+			<h2 className={`img-screen-text ${opacity != 1 ? 'dot-animation' : 'pump-animation'}`}>{text}</h2>
 			{backButton && <NavigationButton position="fixed" left="30px" bottom="30px" text="&larr;" onClick={onclick} />}
 		</div>
 	);
